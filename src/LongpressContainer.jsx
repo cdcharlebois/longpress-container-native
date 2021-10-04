4f9066cc8de8dc9a19ae8af264c35d2a2501edee
Conner Charlebois <cdcharlebois@gmail.com>
2021-10-04 10:30:48 -0400

    add regular press event and opacity

--- src/LongpressContainer.jsx
+++ src/LongpressContainer.jsx
@@ -3,11 +3,26 @@ import { Pressable } from "react-native";
 
 import { HelloWorld } from "./components/HelloWorld";
 
-export const LongpressContainer = ({ content, action }) => {
-    const executeAction = () => {
+export const LongpressContainer = ({ content, action, onPress }) => {
+    const handleLongPress = () => {
+        executeAction(action);
+    };
+    const handlePress = () => {
+        executeAction(onPress);
+    };
+    const executeAction = action => {
         if (action && action.canExecute) {
             action.execute();
         }
     };
-    return <Pressable onLongPress={executeAction}>{content}</Pressable>;
+    return (
+        <Pressable
+            style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1.0 }]}
+            android_ripple = {{radius: 100}}
+            onLongPress={handleLongPress}
+            onPress={handlePress}
+        >
+            {content}
+        </Pressable>
+    );
 };
