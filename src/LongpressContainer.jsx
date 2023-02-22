ab593d497650b43c29c815d74537cd176604b935
Conner Charlebois <cdcharlebois@gmail.com>
2023-02-22 14:31:39 -0500

    reorder event handlers

--- src/LongpressContainer.jsx
+++ src/LongpressContainer.jsx
@@ -1,24 +1,24 @@
 import { Component, createElement } from "react";
 import { Pressable } from "react-native";
 
-import { HelloWorld } from "./components/HelloWorld";
+// import { HelloWorld } from "./components/HelloWorld";
 
 export const LongpressContainer = ({ content, action, onPress }) => {
+    const executeAction = act => {
+        if (act && act.canExecute) {
+            act.execute();
+        }
+    };
     const handleLongPress = () => {
         executeAction(action);
     };
     const handlePress = () => {
         executeAction(onPress);
     };
-    const executeAction = action => {
-        if (action && action.canExecute) {
-            action.execute();
-        }
-    };
     return (
         <Pressable
             style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1.0 }]}
-            android_ripple = {{radius: 100}}
+            android_ripple={{ radius: 100 }}
             onLongPress={handleLongPress}
             onPress={handlePress}
         >
